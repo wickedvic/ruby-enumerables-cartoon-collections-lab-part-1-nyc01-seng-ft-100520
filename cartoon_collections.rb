50f98b4b6628226225fa5053e183c840b09af6b7
Vivek Sanghvi <viksipart@gmail.com>
2020-10-01 16:13:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,11 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  array.each do |ele|
+    if ele == ""
+      puts "Hello Dopey!"
+    end
+  end
 end
 
 def list_dwarves(array)
